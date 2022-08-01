b35d0e95bf1678aa2c6f386c12e70b0cf5749c67
durotemy <durotemy@gmail.com>
2022-08-01 22:19:52 +0100

    first commit

--- backend/src/controller/userController.ts
+++ backend/src/controller/userController.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 
 export const mtn = async (req: Request, res: Response) => {
-  res.send("it is working");
+  res.send("it works");
   console.log("this contoller works");
 };
