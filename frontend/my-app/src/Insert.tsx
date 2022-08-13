83e0f6e7022a1c3632e8a760d863c56c18c770b7
durotemy <durotemy@gmail.com>
2022-08-13 23:06:36 +0100

    styling the frontend

--- frontend/my-app/src/Insert.tsx
+++ frontend/my-app/src/Insert.tsx
@@ -47,29 +47,26 @@ function App() {
         return data;
     }
     return (
-        <div>
-            <div className="lines">
-                {network === "Airtel" ? <img src={airtel} alt="airtel" /> : null}
-                {network === "GLO" ? <img src={glo} alt="glo" /> : null}
-                {network === "MTN" ? <img src={mtn2} alt="mtn" /> : null}
-                {network === "NTEL" ? <img src={ntel} alt="ntel" /> : null}
-                {network === "ETISALAT" ? <img src={etisalat} alt="etisalat" /> : null}
-            </div>
-
-            <h2>welcome{data}</h2>
-            <form onSubmit={handleSubmit}>
-                <label>
+        <div className="container">
+            <div className="form-area">
+                <form onSubmit={handleSubmit}>
                     <input type="text"
                         value={phoneNumber}
                         name="zip"
                         onChange={handleChange}
                         placeholder="Enter a valid number"
                     />
-                </label>
-                <button type="submit" className="btn">Click</button>
-            </form>
+                    <button type="submit" className="btn">Click</button>
+                </form>
+            </div>
+            <div className="icon-area">
+                {network === "Airtel" ? <img src={airtel} alt="airtel" /> : null}
+                {network === "GLO" ? <img src={glo} alt="glo" /> : null}
+                {network === "MTN" ? <img src={mtn2} alt="mtn" /> : null}
+                {network === "NTEL" ? <img src={ntel} alt="ntel" /> : null}
+                {network === "ETISALAT" ? <img src={etisalat} alt="etisalat" /> : null}
+            </div>
         </div>
     );
 }
-
 export default App;
